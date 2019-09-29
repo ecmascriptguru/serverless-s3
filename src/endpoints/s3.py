4edb391cdb192c91db4603ed6b9c16db6f09ba09
Perfect Delivery <ecmascript.guru@gmail.com>
2019-09-29 10:20:09 +0400

    copying objects feature is done.

--- src/endpoints/s3.py
+++ src/endpoints/s3.py
@@ -1,10 +1,40 @@
+import os
 import json
+import boto3
 from src.core.response import return_success
 
 
-def index(event, context):
-    params = event.get('body', 'hello')
+def index(event: dict, context):
+    records = event.get('Records', [])
+    s3 = boto3.client('s3')
+    source_bucket = os.environ.get('INCOMING_BUCKET_NAME')
+    target_bucket = os.environ.get('PROCESSED_BUCKET_NAME')
+    results = list()
 
-    print(event)
-    print(context)
-    return return_success(params)
+    for record in records:
+        item = record['s3']
+        if item['bucket']['name'] != source_bucket:
+            print("Bucket mismatch between %s and %s." % (
+                source_bucket, item['bucket']['name']
+            ))
+            continue
+        key = item['object']['key']
+        copy_source = {
+            'Bucket': source_bucket,
+            'Key': key
+        }
+        try:
+            s3.copy_object(Bucket=target_bucket, Key=key, CopySource=copy_source)
+            results.append({
+                'object': copy_source,
+                'success': True
+            })
+            s3.delete_object(Bucket=source_bucket, Key=key)
+        except Exception as e:
+            print(str(e))
+            results.append({
+                'object': copy_source,
+                'success': False,
+                'msg': str(e)
+            })
+    return return_success(results)
